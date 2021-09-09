4367fc0a09435e051b55881fe5cf53abb915120b
Brian Campbell <bh.campbell@gmail.com>
2021-09-08 20:30:10 -0700

    Applied black formatting.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
- 
+
 class CategoriesInline(admin.TabularInline):
     model = Category.posts.through
 
+
 class PostAdmin(admin.ModelAdmin):
     inlines = [
-            CategoriesInline, 
+        CategoriesInline,
     ]
+
 
 class CategoryAdmin(admin.ModelAdmin):
     inlines = [
-            CategoriesInline,
+        CategoriesInline,
     ]
-    exclude = ('posts',)
+    exclude = ("posts",)
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
